18c04c3ca1ef0b8f2233ceb0891a598d55cfb205
Bárbara Ann Lima <babiplima@gmail.com>
2021-08-24 14:56:34 -0300

    Primeiro Exemplo de hoje 24/08

---
+++ src/br/com/zup/Main.java
@@ -0,0 +1,26 @@
+package br.com.zup;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int Numero1 = 20;
+      int Numero2 = 20;
+
+      // Criando divisão
+
+        int resultadoDaDivisao = Numero1 / Numero2;
+
+      // Criando multiplicação
+
+        int resultadoDaMultiplicacao = Numero1 * Numero2;
+
+      //Exibindo a operação de divisão
+
+        System.out.println("O resultado da divisão é  " + resultadoDaDivisao);
+
+        //Exibindo a operação de multiplicação
+
+        System.out.println("O resultado da é  multiplicação  " +  resultadoDaMultiplicacao);
+
+    }
+}
